5be1ddff4476179f14272b1d4167eaf3c077ff80
Fernando Falcao <fernando.falcao@desjardins.com>
2018-08-10 08:55:47 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const routes = {};
+routes.testroute = require('./routes/testroute');
+
+const server = Hapi.server({
+    host: 'localhost',
+    port: 8080
+});
+
+server.route(routes.testroute);
+
+async function start() {
+    try {
+        await server.start();
+    }
+
+    catch (err) {
+        console.log(err);
+        process.exit(1);    
+    }
+
+    console.log('Server running at: ', server.info.uri);    
+};
+
+start();
+
+
+
