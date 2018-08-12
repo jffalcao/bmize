699e4576c68e21e599c87cbef9a498febd5b3f06
José Fernando Ribeiro Falcao <jfribeirofalcao@gmail.com>
2018-08-12 12:05:28 -0400

    Up to date august 11

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-// Code from solution exercise 00_07.js
-
-var Hapi = require('hapi');
-
-var server = new Hapi.Server();
-server.connection({ port: 8080 });
-
-server.route([
-  {
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-      reply('Hello world from hapi');
-    }
-  }
-]);
-
-server.start(function(err) {
-  console.log('Hapi is listening to http://localhost:8080');
-});
-=======
 'use strict';
 
 const Hapi = require('hapi');
@@ -48,4 +26,3 @@ async function start() {
 };
 
 start();
->>>>>>> 48aa3b01c6ef62b271d21c793006246a490a391f
